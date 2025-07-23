88eb17c1e809876391527908ccbd7a4dc2def096
Vikash Gupta <vikasharmy811@gmail.com>
2025-07-23 19:29:56 +0530

    #fix

--- Lecture4/TriangularPatterns/invTri.cpp
+++ Lecture4/TriangularPatterns/invTri.cpp
@@ -10,11 +10,14 @@ int main(){
     cout<<"-----------------\n";
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <=i; j++)
+        for (int j = 0; j <i; j++)
         {
             cout<<" ";
-            cout <<i;
+            
             sp++;
+        }
+        for(int j=0;j<n-i;j++){
+            cout << (i+1);
         }
         cout << endl;
     }
