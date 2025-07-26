8fd7f9a4380679ad5cadf43c7aad7833e64f2a70
Vikash Gupta <vikasharmy811@gmail.com>
2025-07-26 16:54:04 +0530

    Lol Logic Luck

--- Lecture5/function.cpp
+++ Lecture5/function.cpp
@@ -8,8 +8,13 @@ void printHello(){
 void printNum(int a){
     cout << a;
 }
+void printMul(int a, int b){
+    int mul= a*b;
+    cout << mul<<endl;
+}
 
 int main(){
+    printMul(6,3);
     printHello();
     printNum(5);
     return 0;
