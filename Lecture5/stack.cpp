9d80a4419a68c1f4bcd40c5e207f4484e49d06a9
Vikash Gupta <vikasharmy811@gmail.com>
2025-07-28 20:15:46 +0530

    Lol

--- Lecture5/stack.cpp
+++ Lecture5/stack.cpp
@@ -10,3 +10,28 @@ Stacking ...
 |___________|
 |___________|
 */
+
+#include <iostream>
+
+using namespace std;
+
+void sumN(int n){
+    int sum=0;
+for (int i = 0; i <= n; i++)
+{
+     sum=sum+i;
+}
+cout << sum;
+}
+void fun(){
+    int x=25;
+    cout << x;
+}
+int main(){
+    int a;
+    cout<<"Enter the range: ";
+    cin >> a;
+    sumN(a);
+    fun();
+    return 0;
+}
