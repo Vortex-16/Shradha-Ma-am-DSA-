8d4d2821a1a260e4785b2ecd24f1eee60f11599d
Vikash Gupta <vikasharmy811@gmail.com>
2025-08-27 21:52:31 +0530

    FIx

--- ALPHA5/Sorting/inbuilt.java
+++ ALPHA5/Sorting/inbuilt.java
@@ -1,19 +1,6 @@
 package Sorting;
 import java.util.*;
-public class selessctionSort {
-    public static void sel(int arr[]) {
-        for (int i = 0; i < arr.length-1; i++) {
-            int minPos=i;
-            for(int j= i+1;j<arr.length;j++){
-                if(arr[minPos]>arr[j]){
-                    minPos=j;
-                }
-            }
-            int temp=arr[minPos];
-            arr[minPos]=arr[i];
-            arr[i]=temp;
-        }
-    }
+public class inbuilt {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter array size: ");
@@ -23,10 +10,16 @@ public class selessctionSort {
         for (int i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
-        sel(arr);
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
+        Arrays.sort(arr,3,5);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
         sc.close();
     }
 }
