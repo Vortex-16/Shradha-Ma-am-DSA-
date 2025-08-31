bc46273e8c90ba104a0584495c3f271b868b0ea5
Vikash Gupta <vikasharmy811@gmail.com>
2025-08-31 12:55:14 +0530

    ongoging

--- ALPHA5/OOPs/autoType.py
+++ ALPHA5/OOPs/autoType.py
@@ -9,15 +9,13 @@ time.sleep(5)
 # Some random Python code snippets to "type"
 snippets = [
     "print('Hello, World!')",
-    "for i in range(5): print(i)",
-    "def greet(name): return f'Hello {name}'",
-    "if True:\n    print('Condition met')",
-    "class Bot:\n    def run(self):\n        print('Running...')"
 ]
 
 # Bot will type 10 random snippets
-for _ in range(800):
+for _ in range(300):
     line = random.choice(snippets)
-    pyautogui.write(line, interval=0.19)  # interval = typing speed
+    pyautogui.write(line, interval=0.29)  # interval = typing speed
     pyautogui.press("enter")
-    time.sleep(0.9)  # pause between lines
+    time.sleep(0.9)  # pause between linesP?Rint('Hello, World!')
+
+
