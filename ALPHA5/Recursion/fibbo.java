7885570d28c156cd4dc74ba4c7fd71aa4d1ab593
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-05 12:02:19 +0530

    MSti

--- ALPHA5/Recursion/fibbo.java
+++ ALPHA5/Recursion/fibbo.java
@@ -2,31 +2,31 @@ package Recursion;
 
 public class fibbo {
     public static void main(String[] args) {
-        //System.out.println(fibbo(5));
-        for (int i = 0; i < 10; i++) {
-            System.out.print(fibboSeries(i) + " ");
-        }
+        System.out.println(fibbo(25));
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.print(fibboSeries(i) + " ");
+        // }
     }
-    // static int fibbo(int n) {
-    //     if (n == 0) {
-    //         return 0;
-    //     }
-    //     if (n == 1) {
-    //         return 1;
-    //     }
-    //     int fnm1 = fibbo(n - 1);
-    //     int fnm2 = fibbo(n - 2);
-    //     int fn = fnm1 + fnm2;
-    //     // or fn = fibbo(n-1) + fibbo(n-2);
-    //     //TC = O(2^n)
-    //     //SC = O(n)  (auxiliary space)
-    //     return fn;
-    // }
+    static int fibbo(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int fnm1 = fibbo(n - 1);
+        int fnm2 = fibbo(n - 2);
+        int fn = fnm1 + fnm2;
+        // or fn = fibbo(n-1) + fibbo(n-2);
+        //TC = O(2^n)
+        //SC = O(n)  (auxiliary space)
+        return fn;
+    }
     //fibo series = 0,1,1,2,3,5,8,13,21,34...
-    static int fibboSeries(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return fibboSeries(n - 1) + fibboSeries(n - 2);
-    }
+    // static int fibboSeries(int n) {
+    //     if (n <= 1) {
+    //         return n;
+    //     }
+    //     return fibboSeries(n - 1) + fibboSeries(n - 2);
+    // }
 }
