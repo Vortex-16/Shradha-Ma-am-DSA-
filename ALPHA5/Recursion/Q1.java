92ddc8a05228f347790eb2beab99a4aa24d0c242
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-05 11:43:02 +0530

    Gochaa

--- ALPHA5/Recursion/Q1.java
+++ ALPHA5/Recursion/Q1.java
@@ -3,14 +3,15 @@ package Recursion;
 public class Q1 {
     public static void main(String[] args) {
         factorial(5);
+        System.out.println(factorial(5));
     }
     static int factorial(int n) {
         if (n == 0) {
             return 1;
         }
         int fnm1 = factorial(n - 1);
-        int fn = n * fnm1;
-        System.out.println(fn);
+        int fn = n * factorial(n-1);
+        // or fn = n * fnm1;
         return fn;
     }
 }
