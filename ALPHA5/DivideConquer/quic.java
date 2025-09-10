cae4eea118a1814c2bbeff77f387694daee4bb22
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-10 20:16:02 +0530

    Nothing!!

--- ALPHA5/DivideConquer/quic.java
+++ ALPHA5/DivideConquer/quic.java
@@ -2,7 +2,7 @@ package DivideConquer;
 
 public class quic {
     public static void main(String[] args) {
-        System.out.println("Hello from quic");
+        System.out.println("Hello from quic sort");
         int arr[] = {6, 3, 9, 5, 2, 8};
         int n = arr.length;
         quickSort(arr, 0, n - 1);
