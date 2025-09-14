f4565e56b93f61584fba25733fef9aed2e483eb0
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-14 21:35:33 +0530

    CC

---
+++ ALPHA5/BackTracking/findSubSets.java
@@ -0,0 +1,20 @@
+package BackTracking;
+
+public class findSubSets {
+    public static void findSubsets(String str, int i, String curr){
+        //Base Case
+        if(i == str.length()){
+            System.out.println(curr);
+            return;
+        }
+        //Recursion
+        //Yes choice
+        findSubsets(str, i+1, curr+str.charAt(i));
+        //No choice
+        findSubsets(str, i+1, curr);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, 0, "");
+    }
+}
