d24a3ac7ee732bc4bbb523af5d7d3a832b165006
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-24 22:46:38 +0530

    Example

---
+++ ALPHA5/BackTracking/permutation.java
@@ -0,0 +1,22 @@
+package BackTracking;
+
+public class permutation {
+    
+}
+
+
+/*
+ abc
+ 0 th index
+ a-> bc -> ab -> abc
+           ac -> acb
+ b-> ac -> ba -> bac
+           bc -> bca
+ c-> ab -> ca -> cab
+           cb -> cba
+
+ 1 th index
+ a b-> c
+ c b-> a
+ a c-> b
+ */
