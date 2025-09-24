3ed8e60b888574fe6e206c7351582a4f982c8920
Vikash Gupta <vikasharmy811@gmail.com>
2025-09-24 22:54:35 +0530

    Headache At Peak

--- ALPHA5/BackTracking/permutation.java
+++ ALPHA5/BackTracking/permutation.java
@@ -1,10 +1,25 @@
 package BackTracking;
 
 public class permutation {
+    public static void main(String[] args) {
+        
+    }
+    public static void permute(String str, int l, int r){
+        if(l == r){
+            System.out.println(str);
+            return;
+        }
+        for(int i = l; i <= r; i++){
+            char Curr = str.charAt(i);
+
+            str=str.substring(0, l) + Curr + str.substring(l+1, i) + str.charAt(l) + str.substring(i+1);
+            permute(str, l+1, r);
+        }
+    }
     
 }
 
-
+// Example :
 /*
  abc
  0 th index
@@ -15,3 +30,4 @@ public class permutation {
  c-> ab -> ca -> cab
            cb -> cba
  */
+
