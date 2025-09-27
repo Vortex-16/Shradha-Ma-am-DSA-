6f3ed31a3c4635e4db3052123d201fabf745c28b
Vortex-16 <vikash9c35@gmail.com>
2025-09-27 19:28:18 +0530

    GridWays

---
+++ ALPHA5/BackTracking/gridWays.java
@@ -0,0 +1,21 @@
+package BackTracking;
+
+public class gridWays {
+    public static int gridWays(int i, int j, int n, int m){
+        //Base Case
+        if(i == n-1 && j == m-1){
+            return 1;
+        }
+        if(i >= n || j >= m){
+            return 0;
+        }
+        //Recursion
+        int downWays = gridWays(i+1, j, n, m);
+        int rightWays = gridWays(i, j+1, n, m);
+        return downWays + rightWays;
+    }
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println(gridWays(0, 0, n, m));
+    }
+}
