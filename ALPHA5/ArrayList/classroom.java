ddbd2d349941783296546784245ab3027b080fa9
Vikash Gupta <vikasharmy811@gmail.com>
2025-11-05 13:14:20 +0530

    Launching Done

--- ALPHA5/ArrayList/classroom.java
+++ ALPHA5/ArrayList/classroom.java
@@ -1,5 +1,4 @@
 package ArrayList;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class classroom{
@@ -7,6 +6,16 @@ public class classroom{
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> students = new ArrayList<>();
         ArrayList<Boolean> attendence = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);    
+        students.add("Shradha");
+        students.add("Ma'am");
+        attendence.add(true);
+        attendence.add(false);
+        System.out.println(list);
+        System.out.println(students);
     }
 }
 
