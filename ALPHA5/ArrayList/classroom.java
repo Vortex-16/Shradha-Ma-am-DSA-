96056c085d86d8d10d104a23700c08548862a62e
Vikash Gupta <vikasharmy811@gmail.com>
2025-11-05 13:09:43 +0530

    Definign of array List

---
+++ ALPHA5/ArrayList/classroom.java
@@ -0,0 +1,12 @@
+package ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class classroom{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> students = new ArrayList<>();
+        ArrayList<Boolean> truth = new ArrayList<>();
+    }
+}
+
